151efce1d4561a3bf337fad410fb1b0fa9a3e944
vggog <gluhovkich@gmail.com>
2024-01-01 20:44:05 +0500

    Добавить кнопки на навбар

--- src/components/UI/Navbar/Navbar.jsx
+++ src/components/UI/Navbar/Navbar.jsx
@@ -3,7 +3,11 @@ import MyButton from "../MyButton/MyButton";
 import AuthService from "../../../services/AuthService";
 import MasterAuth from "../../../services/MasterAuth";
 
+import logo from "../../../img/logo.jpg"
+
 import { useNavigate } from "react-router-dom";
+
+import classes from "./Navbar.module.css"
 
 
 function Navbar() {
@@ -19,9 +23,36 @@ function Navbar() {
         navigate('/');
     }
 
+    const currentUrl = window.location.href;
+
     return (
-        <div className="navbar">
+        <div className={classes.navbar}>
+            <img className={classes.logoStyle} src={logo} onClick={() => navigate('/')} alt="Logo"/>
             { localStorage.getItem("whoAuth") && <MyButton style={{'float': 'right'}} onClick={logout}>Выйти</MyButton> }
+            {
+                localStorage.getItem("whoAuth") === "master" && currentUrl.search("/repair_orders/all") === -1 &&
+                <MyButton style={{'float': 'right', 'marginRight': '25px'}} onClick={(e) => {
+                    e.preventDefault();
+                    navigate('/repair_orders/all')
+                }
+            }>Все заказы на ремонт</MyButton>
+            }
+            {
+                localStorage.getItem("whoAuth") === "admin" && currentUrl.search("/admin/master/all") === -1 &&
+                <MyButton style={{'float': 'right', 'marginRight': '25px'}} onClick={(e) => {
+                    e.preventDefault();
+                    navigate('/admin/master/all')
+                }
+            }>Все мастера</MyButton>
+            }
+            {
+                localStorage.getItem("whoAuth") === "admin" && currentUrl.search("/admin/master/add") === -1 &&
+                <MyButton style={{'float': 'right', 'marginRight': '25px'}} onClick={(e) => {
+                    e.preventDefault();
+                    navigate('/admin/master/add')
+                }
+            }>Добавить мастера</MyButton>
+            }
         </div>
     )
 }
